643cc8b2c53de4eee402ce6cc1707756297f96c2
Ranta-Knuuttila Nita <s2401565@BC-TG0W7DYQVW-MacBook-Air.local>
2025-01-22 10:26:03 +0200

    Valmis tehtävä

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,57 @@
+// App.jsx
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+  
+  name: 'Half Stack application development',
+  const: parts = [
+    {
+    name: 'Fundamentals of React',
+    exercises : 10
+  },
+  {
+    name: 'Using props to pass data',
+    exercises : 7
+},
+{
+  name: 'State of a component',
+  exercises : 14
+  }
+  ]
+}
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content parts={parts}/>
+      <Total parts={parts}/>
     </div>
   )
 }
 
+// Header-komponentti
+const Header = (props) => {
+  return <h1>{props.course}</h1>
+}
+
+// Content-komponentti
+const Content = (props) => {
+  return (
+    <div>
+      {props.parts.map((part, index) => (
+        <Part key={index} name={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  )
+}
+
+// Part-komponentti
+const Part = (props) => {
+  return <p>{props.name} {props.exercises}</p>
+}
+
+// Total-komponentti
+const Total = (props) => {
+  return <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+}
 
 export default App
