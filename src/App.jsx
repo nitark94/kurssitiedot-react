505e46c3d3f0cff6354120a3d9187b72b869028d
nitark94 <nita.rantaknuuttila@gmail.com>
2025-03-27 10:51:04 +0200

    tehty asennuksia, joten virheilmoituksia ei enää tule. voidaan siirtyä kohtaan 8

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
-import Course from './components/Course';
-const Course = ({ course }) => {
-  const totalExercises = course.parts.reduce((sum, part) => sum + part.exercises, 0);
+import Course from './components/Course.jsx';
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      { id: 1, name: "Fundamentals of React", exercises: 10 },
+      { id: 2, name: "Using props to pass data", exercises: 7 },
+      { id: 3, name: "State of component", exercises: 14 },
+      { id: 4, name: "Redux", exercises: 11 }
+    ]
+  };
+
+const totalExercises = course.parts.reduce((sum, part) => sum + part.exercises, 0);
 
   return (
     <div>
-      <h2>{course.name}</h2>
-      <ul>
-        {course.parts.map(part => (
-          <li key={part.id}>
-            {part.name} {part.exercises}
-          </li>
-        ))}
-      </ul>
-      <p><strong>Total exercises: {totalExercises}</strong></p>
+      <Course course={course} />
+      <p><strong>Total of {totalExercises} exercises</strong></p>
     </div>
   );
 };
 
-export default Course;
+export default App;
+
+
